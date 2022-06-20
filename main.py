bf0e2715a1eb6029318644bd5fa8c8fcec01a6d0
Disconnect <tankyogurt@gmail.com>
2022-06-20 23:22:31 +0300

    123

---
+++ main.py
@@ -0,0 +1,45 @@
+"""
+import os
+
+from discord.ext import commands
+from dotenv import load_dotenv
+
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.command(name='сасат')
+async def sasat_function(ctx):
+
+    response = "сасат! sasat? сасатъ, САСАТ, сасат?!"
+    await ctx.send(response)
+
+bot.run(TOKEN)
+"""
+import discord
+import os
+from dotenv import load_dotenv
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!тык'):
+        await message.channel.send('!тык')
+
+for k, v in os.environ.items():
+    #print(f'{k}={v}')
+    pass
+print("----------------------___")
+
+load_dotenv()
+client.run(os.getenv('TOKEN'))
