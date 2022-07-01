4cd66bc329b6c403c90781ac81678ade60eb80f5
Disconnect <tankyogurt@gmail.com>
2022-07-01 15:48:43 +0300

    ...

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import discord
+import json
 from discord.ext import commands
 from dotenv import load_dotenv
 load_dotenv()
@@ -9,40 +10,76 @@ client = commands.Bot(command_prefix='!')
 
 
 
+
 # репостит в конкретный канал по команде
 @client.command(name="repost")
 async def bf(ctx, *, args):
     if (ctx.message.author.id == 478866202371031040): # скопировать ID пользователя через ПКМ
-        channal = client.get_channel(991335084986744932)
-        await channal.send(ctx.message.content)
+        channel = client.get_channel(991335084986744932)
+        await channel.send(ctx.message.content)
 
     else:
         await ctx.send(f"Вашего ID = {ctx.message.author.id} нет в списке допущенных для щитпостинга")
 
+
 @client.event
 async def on_message(message):
-    trusted_author = (message.author.id == 478866202371031040)
-    is_DM = (message.channel.type == discord.enums.ChannelType.private)
-    if (trusted_author and is_DM):
-        # await message.channel.send(f'message  from trusted_author and is_DM ')
-        await message.channel.send(f'message reposted')
 
-        channal = client.get_channel(991335084986744932)
-        await channal.send(message.content)
+    if message.author == client.user:  # что бы бот не общался сам с собой
+        return
+
+    trusted_author = author_in_trusted_list(message.author.id)
+    is_DM = (message.channel.type == discord.enums.ChannelType.private)
+
+    if (trusted_author and is_DM):
+
+        c = 0
+
+        for channel_id in channels_to_repost.keys():
+
+            channel = client.get_channel(int(channel_id))
+            # тык # channel = client.get_channel(991335084986744932)
+
+            try:  # пеерсылаем сообщение, поднимем счётчик пересланых сообщений
+                await channel.send(message.content)
+                c += 1
+            except:
+                await message.channel.send(f"Error: can't send to: "
+                                           f"'{channel.guild}   --->   {channel}' channel")
+
+        await message.channel.send(f'message reposted to {c} channels')
+
+
+
+    elif(not trusted_author and is_DM):
+        await message.channel.send(f'Вашего ID нет в моём списке '
+                                   f'допущенных к щитпостингу')
 
 
 def author_in_trusted_list(author):
+    if str(author) in trusted_users.keys():
+        return True
+    else:
+        return False
 
 
-    pass
-
-def get_chanels_for_repost():
-    pass
+def respond_to(message, response):
+    await message.channel.send(response)
 
 
+# ______________________________________________
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
+
+
+file = open('id_users.json', 'r')
+trusted_users = json.load(file)
+file.close()
+
+file = open('channels_to_repost.json', 'r')
+channels_to_repost = json.load(file)
+file.close()
 
 client.run(os.getenv('TOKEN'))
